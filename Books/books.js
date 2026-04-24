8f9cebc8562b6bfbccdf57c73b7dd2b396fdb90a
Ryan Bergman <rbergman80@stuy.edu>
2026-04-24 13:55:25 -0400

    finished all of the europe section in books. Added all of the neccessary transitions and performed some calculations. Added the neccessary resizing. Potential things to look into -> making another resetter based on y position (such so the transforms don't combat with each other). I probably will just make another transformed variable. Next commit will be cleaned up code and America should be finished.

--- Books/books.js
+++ Books/books.js
@@ -3,6 +3,8 @@ let europeHeight = 0;
 let isLoaded = false;
 const flashIterations = 3;
 
+let transformed = false;
+
 function dropdowner() {
     if (!dropdown_active_signal) {
         dropdown_active_signal = true;
@@ -22,11 +24,54 @@ function dropdowner() {
 //I learned 2 things here. you have to check if everything is loaded, and that I should be looking more into the list of events like resize :)
 addEventListener("DOMContentLoaded", () => {
     isLoaded = true;
+
+    multiImageContainerCheck(0);
+    singleImageContainerCheck(0, 1);
     europeHeight = document.getElementById("Europe").getBoundingClientRect().height;
+
+    console.log(document.getElementsByClassName("book_wrapper")[1].getBoundingClientRect().width);
     addEventListener("resize", () => {
+
+        console.log(document.getElementsByClassName("book_wrapper")[1].getBoundingClientRect().width);
+        singleImageContainerCheck(0, 1);
+        multiImageContainerCheck(0);
         europeHeight = document.getElementById("Europe").getBoundingClientRect().height;
     })
 });
+
+function multiImageContainerCheck(index) {
+    if (innerWidth > 1113 && !transformed) {
+        const normalOffset = document.getElementsByClassName("book")[index].getBoundingClientRect().width * 0.15;
+        const missingOffset = (document.getElementsByClassName("book_wrapper")[index].getBoundingClientRect().width - 700) / 2;
+        const offsetToApply = normalOffset + missingOffset;
+        document.getElementsByClassName("flex_image_appear")[index].style.left = offsetToApply + "px";
+    }
+    else if (innerWidth > 1113 && transformed) {
+        const normalOffset = document.getElementsByClassName("book")[index].getBoundingClientRect().width * 0.15;
+        const missingOffset = ((document.getElementsByClassName("book_wrapper")[index].getBoundingClientRect().width * 7 / 9) - 700) / 2;
+        //accounts for extra width by converting the 90% width to what it would be at 70% (7 : 9 ratio)
+        const offsetToApply = normalOffset + missingOffset;
+        document.getElementsByClassName("flex_image_appear")[index].style.left = offsetToApply + "px";
+    }
+    else {
+        document.getElementsByClassName("flex_image_appear")[index].style.left = "15%";
+    }
+}
+
+function singleImageContainerCheck(singleImageIndex, bookIndex) {
+    if (innerWidth > 1113) {
+        const normalOffset = document.getElementsByClassName("book")[bookIndex].getBoundingClientRect().width * 0.1;
+        const missingOffset = ((document.getElementsByClassName("book_wrapper")[bookIndex].getBoundingClientRect().width - 500) * 10 / 40); 
+        //I calculated division by 14 to such so you convert the 7 : 10 -> the change in the book and then dvide by 2, but It appears I was wrong, probably forgot to account for  something.
+        //this is now just empirical division nothing really mathematical here
+        //500 represents the width when the vw starts to kick in if that makes sense - found empirically
+        const offsetToApply = normalOffset + missingOffset;
+        document.getElementsByClassName("single_image")[singleImageIndex].style.right = offsetToApply + "px";
+    }
+    else {
+        document.getElementsByClassName("single_image")[singleImageIndex].style.right = "10%";
+    }
+}
 
 function scrollToBook(bookNumber) {
     switch (bookNumber) {
@@ -73,3 +118,228 @@ function borderFlashing(className, index, count) {
 
     //for loop wasn't working so I made it recursive
 }
+
+function seeMultiImages(index) {
+    transformed = true; 
+
+    document.getElementsByClassName("flex_container_image_clicker")[index].style.animation = "button_swap 1s ease-in-out";
+    document.getElementsByClassName("book_wrapper")[index].style.animation = "multi_image_description_mover 1s ease-in-out";
+
+    document.getElementsByClassName("book_description")[index].style.animation = "text_shrinker 1s ease-in-out";
+    document.getElementsByClassName("book_title")[index].style.animation = "title_shrink 1s ease-in-out";
+
+    setTimeout(() => {
+        document.getElementsByClassName("flex_container_image_clicker")[index].style.left = "-100%";
+
+        document.getElementsByClassName("book_wrapper")[index].style.width = "90%";
+        document.getElementsByClassName("book_wrapper")[index].style.height = "32.5%";
+        document.getElementsByClassName("book_wrapper")[index].style.top = "37.5%";
+        document.getElementsByClassName("book_wrapper")[index].style.left = "5%";
+
+        document.getElementsByClassName("book_description")[index].style.fontSize = " min(1.3vw, 15px)";
+        document.getElementsByClassName("book_title")[index].style.fontSize = "2vw";
+    }, 990)
+
+    document.getElementsByClassName("flex_image_appear")[index].style.animation = "multi_image_appear 2s ease-in-out";
+
+    setTimeout(() => {
+        document.getElementsByClassName("flex_image_appear")[index].style.opacity = "1";
+    }, 1980);
+}
+
+function seeSingleImages(genericIndex, singleIndex) {
+    transformed = true; 
+
+    document.getElementsByClassName("flex_container_image_clicker")[genericIndex].style.animation = "button_swap 1s ease-in-out";
+    document.getElementsByClassName("single_book_wrapper")[singleIndex].style.animation = "single_image_description_mover 1s ease-in-out";
+
+    document.getElementsByClassName("book_description")[genericIndex].style.animation = "single_text_grower 1s ease-in-out";
+    document.getElementsByClassName("book_title")[genericIndex].style.animation = "single_title_grower 1s ease-in-out";
+
+    setTimeout(() => {
+        document.getElementsByClassName("flex_container_image_clicker")[genericIndex].style.left = "-100%";
+
+        document.getElementsByClassName("single_book_wrapper")[singleIndex].style.width = "50%";
+        document.getElementsByClassName("single_book_wrapper")[singleIndex].style.height = "70%";
+        document.getElementsByClassName("single_book_wrapper")[singleIndex].style.top = "5%";
+        document.getElementsByClassName("single_book_wrapper")[singleIndex].style.left = "3%";
+
+        document.getElementsByClassName("book_description")[genericIndex].style.fontSize = "font-size: min(2.3vw, 25px);";
+        document.getElementsByClassName("book_title")[genericIndex].style.fontSize = "4vw";
+    }, 990)
+
+    document.getElementsByClassName("single_image")[singleIndex].style.animation = "single_image_appear 2s ease-in-out";
+
+    setTimeout(() => {
+        document.getElementsByClassName("single_image")[singleIndex].style.opacity = "1";
+    }, 1980);
+}
+
+function hideMultiImages(index) {
+    transformed = false;
+
+    document.getElementsByClassName("flex_container_image_clicker")[index].style.animation = "button_swap_back 1s ease-in-out";
+    document.getElementsByClassName("book_wrapper")[index].style.animation = "multi_image_description_mover_back 1s ease-in-out";
+
+    document.getElementsByClassName("book_description")[index].style.animation = "single_text_revert 1s ease-in-out";
+    document.getElementsByClassName("book_title")[index].style.animation = "single_title_revert 1s ease-in-out";
+
+    setTimeout(() => {
+        document.getElementsByClassName("flex_container_image_clicker")[index].style.left = "0";
+
+        document.getElementsByClassName("book_wrapper")[index].style.width = "70%";
+        document.getElementsByClassName("book_wrapper")[index].style.height = "60%";
+        document.getElementsByClassName("book_wrapper")[index].style.top = "10%";
+        document.getElementsByClassName("book_wrapper")[index].style.left = "15%";
+
+        document.getElementsByClassName("book_description")[index].style.fontSize = "min(1.8vw, 23px)";
+        document.getElementsByClassName("book_title")[index].style.fontSize = "3vw";
+    }, 990)
+
+    document.getElementsByClassName("flex_image_appear")[index].style.animation = "multi_image_disappear 2s ease-in-out";
+
+    setTimeout(() => {
+        document.getElementsByClassName("flex_image_appear")[index].style.opacity = "0";
+    }, 1980);
+}
+
+function hideSingleImages(genericIndex, singleIndex) {
+    transformed = true; 
+
+    document.getElementsByClassName("flex_container_image_clicker")[genericIndex].style.animation = "button_swap_back 1s ease-in-out";
+    document.getElementsByClassName("single_book_wrapper")[singleIndex].style.animation = "single_image_description_mover_back 1s ease-in-out";
+
+    document.getElementsByClassName("book_description")[genericIndex].style.animation = "text_revert 1s ease-in-out";
+    document.getElementsByClassName("book_title")[genericIndex].style.animation = "title_revert 1s ease-in-out";
+
+    setTimeout(() => {
+        document.getElementsByClassName("flex_container_image_clicker")[genericIndex].style.left = "0";
+
+        document.getElementsByClassName("single_book_wrapper")[singleIndex].style.width = "70%";
+        document.getElementsByClassName("single_book_wrapper")[singleIndex].style.height = "60%";
+        document.getElementsByClassName("single_book_wrapper")[singleIndex].style.top = "10%";
+        document.getElementsByClassName("single_book_wrapper")[singleIndex].style.left = "15%";
+
+        document.getElementsByClassName("book_description")[genericIndex].style.fontSize = "min(1.8vw, 23px)";
+        document.getElementsByClassName("book_title")[genericIndex].style.fontSize = "3vw";
+    }, 990)
+
+    document.getElementsByClassName("single_image")[singleIndex].style.animation = "single_image_dissapear 2s ease-in-out";
+
+    setTimeout(() => {
+        document.getElementsByClassName("single_image")[singleIndex].style.opacity = "0";
+    }, 1980);
+}
+
+
+
+// @keyframes text_shrinker {
+//     from {
+//         font-size: min(2vw, 25px);
+//     }
+//     to {
+//         font-size: min(1.3vw, 15px);
+//     }
+// }
+
+// @keyframes text_revert {
+//     from {
+//         font-size: min(1.3vw, 15px);
+//     }
+//     to {
+//         font-size: min(2vw, 25px);
+//     }
+// }
+
+// @keyframes title_shrink {
+//     from {
+//         font-size: 3vw;
+//     }
+//     to {
+//         font-size: 2vw;;
+//     }
+// }
+// @keyframes title_revert {
+//     from {
+//         font-size: 2vw;
+//     }
+//     to {
+//         font-size: 3vw;
+//     }
+// }
+// @keyframes multi_image_description_mover {
+//     from {
+//         width: 70%;
+//         height: 60%;
+//         top: 10%;
+//         left: 15%;
+//     }
+
+//     to {
+//         width: 90%;
+//         height: 32.5%;
+//         left: 5%;
+//         top: 37.5%
+//     }
+// }
+
+// @keyframes multi_image_description_mover_back {
+//     from {
+//         width: 90%;
+//         height: 32.5%;
+//         left: 5%;
+//         top: 37.5%;
+//     }
+
+//     to {
+//         width: 70%;
+//         height: 60%;
+//         top: 10%;
+//         left: 15%;
+//     }
+// }
+
+// @keyframes multi_image_appear {
+//     from {
+//         opacity: 0;
+//     }
+
+//     to {
+//         opacity: 1;
+//     }
+// }
+
+// @keyframes multi_image_disappear {
+//     from {
+//         opacity: 1;
+//     }
+
+//     to {
+//         opacity: 0;
+//     }
+// }
+
+// @keyframes button_swap {
+//     from {
+//         transform: translateX(0%);
+
+//     }
+
+//     to {
+//         transform: translateX(-50%);
+
+//     }
+// }
+
+// @keyframes button_swap_back {
+//     from {
+//         transform: translateX(0%);
+
+//     }
+
+//     to {
+//         transform: translateX(-50%);
+
+//     }
+// }
+
