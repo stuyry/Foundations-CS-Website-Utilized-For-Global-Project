f1b0fd5012c3ce7f170bb4fe21d9f50b8e98209f
Ryan Bergman <rbergman80@stuy.edu>
2026-04-24 00:43:47 -0400

    Implemented Window.scrollTo(), mathed it out using percents and everything. Math was required because I realized I should be scaling the height by vw and having it cap at a certain amount of pixels such so I don't need to deal with font formatting and etc. So that required me to use the field innerwidth and make some calculations. Started implementing this dynamic height system. -> TODO: IMPLEMENT IT EVERYWHERE BEFORE. Some places like the spinning title areas I let it be because it looks better. started working on the book description sections - forgot to commit before starting. -> learned some cool things in the js like DomContentLoaded and the resize event

--- Books/books.js
+++ Books/books.js
@@ -1,4 +1,7 @@
 let dropdown_active_signal = false;
+let europeHeight = 0;
+let isLoaded = false;
+const flashIterations = 3;
 
 function dropdowner() {
     if (!dropdown_active_signal) {
@@ -14,4 +17,59 @@ function dropdowner() {
             document.getElementById("dropdown_container").style.display = "none";
         }, 490); //this is corresponding to the animation time! -> if you change one, you must change the other
     }
+}
+
+//I learned 2 things here. you have to check if everything is loaded, and that I should be looking more into the list of events like resize :)
+addEventListener("DOMContentLoaded", () => {
+    isLoaded = true;
+    europeHeight = document.getElementById("Europe").getBoundingClientRect().height;
+    addEventListener("resize", () => {
+        europeHeight = document.getElementById("Europe").getBoundingClientRect().height;
+    })
+});
+
+function scrollToBook(bookNumber) {
+    switch (bookNumber) {
+        case 0:
+            scrollTo(0, 1000 + (innerWidth * 0.3) + 300 + (europeHeight * 0.04));
+            break;
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+    }
+
+    borderFlashing("book_wrapper", bookNumber, 0);
+}
+
+//not my proudest moment :p 
+function borderFlashing(className, index, count) {
+    if (count == flashIterations) {
+        return;
+    }
+
+    document.getElementsByClassName(className)[index].style.animation = "borderFlashIn 1s ease-in-out";
+    setTimeout(() => {
+        document.getElementsByClassName(className)[index].style.border = "solid 6px yellow";
+
+        document.getElementsByClassName(className)[index].style.removeProperty("animation");
+        document.getElementsByClassName(className)[index].style.animation = "borderFlashOut 1s ease-in-out";
+
+        setTimeout(() => {
+            document.getElementsByClassName(className)[index].style.border = "dashed 4px #91837e";
+            document.getElementsByClassName(className)[index].style.removeProperty("animation");
+        }, 990);
+
+    }, 990);
+
+    setTimeout(() => {
+        borderFlashing(className, index, count + 1);
+    }, 2000)
+
+    //for loop wasn't working so I made it recursive
 }
