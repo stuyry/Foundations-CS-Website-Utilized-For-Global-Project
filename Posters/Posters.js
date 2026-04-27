b846961b3e57879c343381e1f189ec7b0a4d0d6b
Ryan Bergman <rbergman80@stuy.edu>
2026-04-27 12:56:36 -0400

    cleaned up posters A LOT -> sizing now looks much better. I learned how to animate in javascript such so i can use the values i obtain in the animations. other than that i had to combat with the fill attribute in the animation. I didn't realize that was the culprit for why the containers wouldnt respond to the centerer method. i will come back and clean it up using OOP.

--- Posters/Posters.js
+++ Posters/Posters.js
@@ -1,6 +1,14 @@
 let dropdown_active_signal = false;
-let imageClicked = false;
-let imageClickedRight = false;
+
+let imageClickedLeft1 = false;
+let imageClickedLeft2 = false;
+let imageClickedRight1 = false;
+let imageClickedRight2 = false;
+
+let leftWidth1;
+let rightWidth1;
+let leftWidth2;
+let rightWidth2;
 
 function dropdowner() {
     if (!dropdown_active_signal) {
@@ -18,8 +26,30 @@ function dropdowner() {
     }
 }
 
+addEventListener("resize", () => {
+    variableSetter();
+
+    image_clicker_centerer();
+});
+
+addEventListener("DOMContentLoaded", () => {
+    variableSetter();
+
+    image_clicker_centerer();
+});
+
+setInterval(() => {
+    variableSetter();
+
+   image_clicker_centerer();
+}, 1000)
+
 //for later, make an array of every left side and right side image mover instance then loop through those numbers and reset at certain points. but for this small webPAGE it should suffice
 addEventListener("scroll", () => {
+    variableSetter();
+
+    image_clicker_centerer();
+    
     if (window.scrollY > 500 && window.scrollY < 800) {
         document.getElementById("duck").style.animation = "duck_animation 1s ease-in-out";
         setTimeout(() => {
@@ -27,20 +57,19 @@ addEventListener("scroll", () => {
         }, 990);
     }
 
-    if (window.scrollY > 2600) {
-       imageClicked = true;
-       image_mover_left(0, 0);
-       imageClickedRight = true;
-       image_mover_right(0, 1);
-    }
+    // if (window.scrollY > 2600) {
+    //     imageClicked = true;
+    //     image_mover_left1();
+    //     imageClickedRight = true;
+    //     image_mover_right1();
+    // }
 
-    if (window.scrollY < 2600) {
-        imageClicked = true;
-       image_mover_left(1, 2);
-       imageClickedRight = true;
-       image_mover_right(1, 3);
-    }
-
+    // if (window.scrollY < 2600) {
+    //     imageClicked = true;
+    //     image_mover_left2();
+    //     imageClickedRight = true;
+    //     image_mover_right2();
+    // }
 });
 
 // function login_checker() {
@@ -62,46 +91,221 @@ function key_checker() {
 }
 
 //learned that when you use classNames you must specify the element you are using as an array index -> makes good sense to me
-function image_mover_left(index, clickerIndex) {
-    if (!imageClicked) {
-        document.getElementsByClassName("image_tooltip_section")[index].style.animation = "see_image 1s ease-in-out";
+function image_mover_left1(index = 0, clickerIndex = 0) {
+    if (!imageClickedLeft1) { //this is where the animation updater should be called!!!!!
+        document.getElementsByClassName("image_tooltip_section")[index].animate([
+            { left: (-1 * leftWidth1) + "px" }, // from
+            { left: 0 + "px" }          // to
+        ], {
+            duration: 1000,
+            easing: "ease-in-out",
+            
+        });
         document.getElementsByClassName("image_clicker")[clickerIndex].style.animation = "rotate_arrow 1s ease-in-out";
-        imageClicked = true;
+        imageClickedLeft1 = true;
 
         setTimeout(() => {
-            document.getElementsByClassName("image_tooltip_section")[index].style.left = "0px";
+            image_clicker_centerer();
             document.getElementsByClassName("image_clicker")[clickerIndex].style.transform = "rotateZ(180deg)";
         }, 990);
     }
     else {
-        document.getElementsByClassName("image_tooltip_section")[index].style.animation = "see_image_out 1s ease-in-out";
+        document.getElementsByClassName("image_tooltip_section")[index].animate([
+    { left: 0 + "px" },
+    { left: (-1 * leftWidth1) + "px" }
+], {
+    duration: 1000,
+    easing: "ease-in-out",
+    
+});
         document.getElementsByClassName("image_clicker")[clickerIndex].style.animation = "rotate_arrow_back 1s ease-in-out";
-        imageClicked = false;
+        imageClickedLeft1 = false;
         setTimeout(() => {
-            document.getElementsByClassName("image_tooltip_section")[index].style.left = "-345px";
+            image_clicker_centerer();
             document.getElementsByClassName("image_clicker")[clickerIndex].style.transform = "rotateZ(0deg)";
         }, 990);
     }
 }
 
-function image_mover_right(index, clickerIndex) {
-    if (!imageClickedRight) {
-        document.getElementsByClassName("right_image_tooltip_section")[index].style.animation = "see_image_right 1s ease-in-out";
-        document.getElementsByClassName("image_clicker")[clickerIndex].style.animation = "rotate_arrow_back 1s ease-in-out";
-        imageClickedRight = true;
+function image_mover_left2(index = 1, clickerIndex = 2) {
+    if (!imageClickedLeft2) { //this is where the animation updater should be called!!!!!
+        document.getElementsByClassName("image_tooltip_section")[index].animate([
+            { left: (-1 * leftWidth2) + "px" }, // from
+            { left: 0 + "px" }          // to
+        ], {
+            duration: 1000,
+            easing: "ease-in-out",
+            
+        });
+        document.getElementsByClassName("image_clicker")[clickerIndex].style.animation = "rotate_arrow 1s ease-in-out";
+        imageClickedLeft2 = true;
 
         setTimeout(() => {
-            document.getElementsByClassName("right_image_tooltip_section")[index].style.right = "0px";
+            image_clicker_centerer();
+            document.getElementsByClassName("image_clicker")[clickerIndex].style.transform = "rotateZ(180deg)";
+        }, 990);
+    }
+    else {
+        document.getElementsByClassName("image_tooltip_section")[index].animate([
+    { left: 0 + "px" },
+    { left: (-1 * leftWidth2) + "px" }
+], {
+    duration: 1000,
+    easing: "ease-in-out",
+   
+});
+        document.getElementsByClassName("image_clicker")[clickerIndex].style.animation = "rotate_arrow_back 1s ease-in-out";
+        imageClickedLeft2 = false;
+        setTimeout(() => {
+            image_clicker_centerer();
+            document.getElementsByClassName("image_clicker")[clickerIndex].style.transform = "rotateZ(0deg)";
+        }, 990);
+    }
+}
+
+function image_mover_right1(index = 0, clickerIndex = 1) {
+    if (!imageClickedRight1) {
+        document.getElementsByClassName("right_image_tooltip_section")[index].animate([
+            { left: (innerWidth) + "px" }, // from
+            { left: innerWidth - rightWidth1  + "px" }          // to
+        ], {
+            duration: 1000,
+            easing: "ease-in-out",
+            
+        });
+        document.getElementsByClassName("image_clicker")[clickerIndex].style.animation = "rotate_arrow_back 1s ease-in-out";
+        imageClickedRight1 = true;
+
+        setTimeout(() => {
+            image_clicker_centerer();
             document.getElementsByClassName("image_clicker")[clickerIndex].style.transform = "rotateZ(0deg)";
         }, 990);
     }
     else {
-        document.getElementsByClassName("right_image_tooltip_section")[index].style.animation = "see_image_right_out 1s ease-in-out";
+        document.getElementsByClassName("right_image_tooltip_section")[index].animate([
+            { left:  innerWidth - rightWidth1 + "px" }, // from
+            { left: innerWidth + "px" }          // to
+        ], {
+            duration: 1000,
+            easing: "ease-in-out",
+            
+        });
         document.getElementsByClassName("image_clicker")[clickerIndex].style.animation = "rotate_arrow 1s ease-in-out";
-        imageClickedRight = false;
+        imageClickedRight1 = false;
         setTimeout(() => {
-            document.getElementsByClassName("right_image_tooltip_section")[index].style.right = "-345px";
+            image_clicker_centerer();
             document.getElementsByClassName("image_clicker")[clickerIndex].style.transform = "rotateZ(180deg)";
         }, 990);
     }
 }
+
+function image_mover_right2(index = 1, clickerIndex = 3) {
+     if (!imageClickedRight2) {
+        document.getElementsByClassName("right_image_tooltip_section")[index].animate([
+            { right: -1 * rightWidth2 + "px" }, // from
+            { right: 0 + "px" }          // to
+        ], {
+            duration: 1000,
+            easing: "ease-in-out",
+            
+        });
+        document.getElementsByClassName("image_clicker")[clickerIndex].style.animation = "rotate_arrow_back 1s ease-in-out";
+        imageClickedRight2 = true;
+
+        setTimeout(() => {
+            image_clicker_centerer();
+            document.getElementsByClassName("image_clicker")[clickerIndex].style.transform = "rotateZ(0deg)";
+        }, 990);
+    }
+    else {
+        document.getElementsByClassName("right_image_tooltip_section")[index].animate([
+            { right: 0 + "px" }, // from
+            { right: -1 * rightWidth2  + "px" }          // to
+        ], {
+            duration: 1000,
+            easing: "ease-in-out",
+            
+        });
+        document.getElementsByClassName("image_clicker")[clickerIndex].style.animation = "rotate_arrow 1s ease-in-out";
+        imageClickedRight2 = false;
+        setTimeout(() => {
+            image_clicker_centerer();
+            document.getElementsByClassName("image_clicker")[clickerIndex].style.transform = "rotateZ(180deg)";
+        }, 990);
+    }
+}
+
+
+function variableSetter() {
+    leftWidth1 = document.getElementsByClassName("image_tooltip_section")[0].getBoundingClientRect().width;
+    leftWidth2 = document.getElementsByClassName("image_tooltip_section")[1].getBoundingClientRect().width;
+    rightWidth1 = document.getElementsByClassName("right_image_tooltip_section")[0].getBoundingClientRect().width;
+    rightWidth2 = document.getElementsByClassName("right_image_tooltip_section")[1].getBoundingClientRect().width;
+}
+
+function image_clicker_centerer() {
+
+    if (imageClickedLeft1) {
+        document.getElementsByClassName("image_tooltip_section")[0].style.left = 0 + "px";
+    } 
+    else {
+        document.getElementsByClassName("image_tooltip_section")[0].style.left = (-1 * leftWidth1) + "px";
+    }
+
+    if(imageClickedLeft2) {
+        document.getElementsByClassName("image_tooltip_section")[1].style.left = 0 + "px";
+    } else {
+        document.getElementsByClassName("image_tooltip_section")[1].style.left = (-1 * leftWidth2) + "px";
+    }
+
+    if (imageClickedRight1) {
+
+    document.getElementsByClassName("right_image_tooltip_section")[0].style.right =  0 + "px";
+    } else {
+    document.getElementsByClassName("right_image_tooltip_section")[0].style.right = -1* rightWidth1 + "px";
+    }
+
+    if (imageClickedRight2) {
+    document.getElementsByClassName("right_image_tooltip_section")[1].style.right =  0 + "px";
+    } else {
+    document.getElementsByClassName("right_image_tooltip_section")[1].style.right =  -1 * rightWidth1 + "px";
+    }
+            
+    
+}
+
+// function transformRightClickerWidth(width) {
+//     return (innerWidth / 1.455) + width;
+// }
+
+// function update_clicker_animations() {
+//     const stylesheeter = document.styleSheets[5];
+//     const see_image_keyframe = stylesheeter.cssRules[0];
+//     const see_image_out_keyframe = stylesheeter.cssRules[1];
+//     const see_image_right_keyframe = stylesheeter.cssRules[4];
+//     const see_image_right_out_keyframe = stylesheeter.cssRules[5];
+
+//     console.log(stylesheeter.href);
+
+//     if (leftWidth1) {
+//         see_image_keyframe.cssRules[0].style.setProperty("left", (-1 * leftWidth1) + "px");
+
+//         see_image_keyframe.cssRules[1].style.setProperty("left", leftWidth1 + "px");
+    
+//         see_image_out_keyframe.cssRules[1].style.setProperty("left", (-1 * leftWidth1) + "px");
+
+//         see_image_out_keyframe.cssRules[0].style.setProperty("left", leftWidth1 + "px");
+//     }
+
+//     if (rightWidth1) {
+//         see_image_right_keyframe.cssRules[0].style.setProperty("left", transformRightClickerWidth(rightWidth1) + "px");
+
+//         see_image_right_keyframe.cssRules[1].style.setProperty("left", (transformRightClickerWidth(rightWidth1) - rightWidth1) + "px");
+    
+//         see_image_right_out_keyframe.cssRules[1].style.setProperty("left", transformRightClickerWidth(rightWidth1) + "px");
+
+//         see_image_right_out_keyframe.cssRules[0].style.setProperty("left", (transformRightClickerWidth(rightWidth1) - rightWidth1) + "px");
+//     }
+
+// }
+
