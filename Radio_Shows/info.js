89fb2920ba42991112e12f909f14988fd6cf8d3e
Ryan Bergman <rbergman80@stuy.edu>
2026-05-01 00:06:41 -0400

    using javascript I implemented navbar and overall dynamic sizing to all elements on the radio.html. This will be continued after I get some school work donw -> needs to be done for everything. THE PROBLEM IT SOLVES: differen resolutions have different ideas of pixels -> therefore for fixed widths they become fractions of the height of the element (wither vw or innerHeight). Initially I was going to do it based off of width but using ratios with the widhts was not ideal because it still wasnt resizing dynamically

--- Radio_Shows/info.js
+++ Radio_Shows/info.js
@@ -1,42 +1,58 @@
 let dropdown_active_signal = false;
-const referenceHeight = screen.availHeight;
+const referenceHeight = innerHeight;
 
 
 function dropdowner() {
     if (!dropdown_active_signal) {
         dropdown_active_signal = true;
-        document.getElementById("dropdown_container").style.animation = "dropdown_animation_in 0.5s ease-in-out";
-        document.getElementById("dropdown_container").style.display = "inline";
 
+        document.getElementById("dropdown_formatter").animate([
+            {
+                top: `${referenceHeight * 0.08}px`,
+                opacity: "0"
+            },
+            {
+                top: `${referenceHeight * 0.15}px`,
+                opacity: "1"
+            }
+        ], { duration: 500, easing: "ease-in-out", fill: "forwards" });
     }
+
     else {
         dropdown_active_signal = false;
-        document.getElementById("dropdown_container").style.animation = "dropdown_animation_out 0.5s ease-in-out";
-        setTimeout(() => {
-            document.getElementById("dropdown_container").style.display = "none";
-        }, 490); //this is corresponding to the animation time! -> if you change one, you must change the other
+        document.getElementById("dropdown_formatter").animate([
+            {
+                top: `${referenceHeight * 0.15}px`,
+                opacity: "1"
+            },
+            {
+                top: `${referenceHeight * 0.08}px`,
+                opacity: "0"
+            }
+        ], { duration: 500, easing: "ease-in-out", fill: "forwards" });
+
     }
 }
 
 addEventListener("resize", () => {
-  video_centerer();
+    video_centerer();
 });
 
 addEventListener("scroll", () => {
-  video_centerer();
+    video_centerer();
 });
 
 addEventListener("scroll", () => {
     const navbarWidth = document.getElementById("navbar").getBoundingClientRect().width;
-    
-    const startPos = navbarWidth * 0.1;       
-    const endPos = navbarWidth * 0.8;          
+
+    const startPos = navbarWidth * 0.1;
+    const endPos = navbarWidth * 0.8;
     const distance = endPos - startPos;
 
-    const totalHeight = document.getElementById("title_section").getBoundingClientRect().height + 
-    document.getElementsByClassName("video_section")[0].getBoundingClientRect().height +
-    document.getElementById("footer").getBoundingClientRect().height +
-    document.getElementsByClassName("video_section")[1].getBoundingClientRect().height;
+    const totalHeight = document.getElementById("title_section").getBoundingClientRect().height +
+        document.getElementsByClassName("video_section")[0].getBoundingClientRect().height +
+        document.getElementById("footer").getBoundingClientRect().height +
+        document.getElementsByClassName("video_section")[1].getBoundingClientRect().height;
 
     const progress = Math.min(scrollY / totalHeight, 1);
     console.log(scrollY);
@@ -46,18 +62,18 @@ addEventListener("scroll", () => {
     document.getElementById("navbar_duck").style.left = `${application}px`;
 });
 function video_centerer() {
-  const iterations = document.getElementsByClassName("video").length;
+    const iterations = document.getElementsByClassName("video").length;
 
-  
-    const application = 
-    (innerWidth -
-    document.getElementsByClassName("video")[1].getBoundingClientRect().width) / 2;
+
+    const application =
+        (innerWidth -
+            document.getElementsByClassName("video")[1].getBoundingClientRect().width) / 2;
 
     document.getElementsByClassName("video")[1].style.left = application + "px"
-     document.getElementsByClassName("video")[0].style.left = application + "px"
-  
+    document.getElementsByClassName("video")[0].style.left = application + "px"
 
-  //first try hell yeah -> I learned from my messy mistakes in books which i will change later :p
+
+    //first try hell yeah -> I learned from my messy mistakes in books which i will change later :p
 }
 
 
@@ -79,11 +95,37 @@ setInterval(() => { //nested in here because the display is none
     });
 
     setHeights();
+    setWidths();
 }, 100);
 
 function setHeights() {
     let application = Math.min(innerWidth, referenceHeight);
-    for(let i = 0; i < document.getElementsByClassName("video_section").length; i++) {
+    for (let i = 0; i < document.getElementsByClassName("video_section").length; i++) {
         document.getElementsByClassName("video_section")[i].style.height = `${application}px`;
     }
+
+    document.getElementById("title_section").style.height = `${application}px`;
+
+    document.getElementById("navbar").style.height = `${referenceHeight * 0.08}px`;
+
+    document.getElementById("dropdown_formatter").style.height = `${referenceHeight * 0.13}px`;
+
+    document.getElementById("dropdown_formatter").style.top = `${referenceHeight * 0.15}px`;
+
+    centerNavbar();
+}
+
+function setWidths() {
+    let musicImageApplication = Math.min(innerWidth, document.getElementById("title_section").getBoundingClientRect().height * 0.45);
+    document.getElementById("music_intro_image_container").style.width = `${musicImageApplication}px`;
+    document.getElementsByClassName("video")[0].style.width = `${musicImageApplication}px`;
+    document.getElementsByClassName("video")[1].style.width = `${musicImageApplication}px`;
+
+}
+
+
+
+function centerNavbar() {
+    let application = (innerWidth - document.getElementById("dropdown_formatter").getBoundingClientRect().width) / 2;
+    document.getElementById("dropdown_formatter").style.left = `${application}px`;
 }
